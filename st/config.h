a456b5d6b53c67676a4218a1c5ca886f566e4f32
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-06-21 22:35:55 +0200

    improve st text visibility

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.45;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
